ce5cfde03710e3a25f576e45833ab044525042dd
RomuloDsouza <0000402106@senaimgaluno.com.br>
2024-01-30 22:35:13 -0300

    atividade

---
+++ Atividade3.js
@@ -0,0 +1,20 @@
+let biblioteca={
+    idAluno:1,
+    nome:'Romulo',
+    nomeLivroslidos:['Joao e Maria','O Código Da Vinci,','A Culpa é das Estrelas','O Hobbit'],
+     emprestimoLivro:[{numeroMatricula: '7877', nomeCurso:'front',
+      dataRetirada:'2023/12/25',dataDevolucao:'2024/01/25'  },],
+
+}
+
+console.log(biblioteca)
+
+let escola = {
+    cursos: { nomeCurso: 'front',  
+},
+    professores: [
+      { nome: "Prof. Silva", disciplina: "Matemática" , cargaHoriasemanal:'24 H' },],
+   alunos: [
+      { nome: "Romulo", idmatricula: "101", clase: "B" }, ],
+        
+    }
